5811b2fd6be499dcbc31b72f0b0c8d9776cadc62
luckycyl <569265915@qq.com>
2021-01-07 09:41:40 +0800

    dd

--- src/main/java/com/cyl/springbootweb/SpringbootWebApplication.java
+++ src/main/java/com/cyl/springbootweb/SpringbootWebApplication.java
@@ -2,12 +2,19 @@ package com.cyl.springbootweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringbootWebApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootWebApplication.class, args);
     }
 
+    @GetMapping
+    public String hello() {
+        return "hello jenkins !";
+    }
 }
